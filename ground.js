938878cc4a0625a83d21a7540382646507efa665
Rajroop-code-blip <85220358+Rajroop-code-blip@users.noreply.github.com>
2021-08-11 19:38:13 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,17 @@
+class Ground
+{
+    display() {
+    groundObj.display;
+
+ groundObj=new ground(width/2,670,width,20);
+  leftSide = new ground(1100,600,20,120);
+  rightSide = new ground(110,600,20,120);
+}
+ }
+
+function keyPressed() {
+    if(keyCode === UP_ARROW){
+    
+    }
+}
+
